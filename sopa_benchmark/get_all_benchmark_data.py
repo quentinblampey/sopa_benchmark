62c1b572d0bebacf429fc528f6c46eb331dd5df8
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2023-11-21 10:38:03 +0100

    print scale

--- sopa_benchmark/get_all_benchmark_data.py
+++ sopa_benchmark/get_all_benchmark_data.py
@@ -3,6 +3,3 @@ from .utils import _get_benchmark_data, get_uniform
 if __name__ == "__main__":
     for scale in range(8):
         get_uniform(1024 * 2**scale)
-
-    for scale in range(8):
-        _get_benchmark_data(1024 * 2**scale)
--- sopa_benchmark/get_all_benchmark_data2.py
+++ sopa_benchmark/get_all_benchmark_data2.py
@@ -2,4 +2,5 @@ from .utils import _get_benchmark_data
 
 if __name__ == "__main__":
     for scale in range(8):
+        print(1024 * 2**scale)
         _get_benchmark_data(1024 * 2**scale)
